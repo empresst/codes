de77a3596d7dd4018831e5ec0ab5c2ad8cc3f206
empresst <50694910+empresst@users.noreply.github.com>
2022-04-08 14:37:47 +0600

    Create max_subarray_sum.cpp

---
+++ max_subarray_sum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int maxSubArraySum(int a[], int size)
+{
+int max_so_far = a[0];
+int curr_max = a[0];
+
+for (int i = 1; i < size; i++)
+{
+		curr_max = max(a[i], curr_max+a[i]);
+		max_so_far = max(max_so_far, curr_max);
+}
+return max_so_far;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+	cout << "Maximum subarray sum: " << maxSubArraySum(a, n)<<"\n";
+    }
+}
